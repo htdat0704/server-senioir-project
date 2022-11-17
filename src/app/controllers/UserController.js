76f3e5012b32b7314a47a9bc46e7fc2025418119
htdat0704 <htdat.18it3@sict.udn.vn>
2022-11-17 11:16:03 +0700

    update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,7 +2,6 @@ const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
 const cloundinary = require("cloudinary");
 const passport = require("passport");
-var fs = require("fs");
 
 const UserService = require("../services/userService");
 const VehicleService = require("../services/vehicleService");
@@ -239,11 +238,10 @@ class UserController {
 
    updateProfile = async (req, res, next) => {
       try {
-         // const { image } = req.files;
-         // req.body.avatar =
-         //    `data:image/png;base64,` + image.data.toString("base64");
-
-         // const user = await UserService.updateProfile(req.user._id, req.body);
+         if (!req.body) {
+            return next(new ErrorHander("Nothing to Update", 402));
+         }
+         const user = await UserService.updateProfile(req.user._id, req.body);
 
          await UserService.addNotification(
             user._id,
